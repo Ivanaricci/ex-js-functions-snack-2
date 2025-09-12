2546a3046ed83bc07471eae247b6cbf3eeb39479
Ivanaricci <ivana.ricci26@gmail.com>
2025-09-12 16:10:54 +0200

    [UPD] Done snack5

--- script.js
+++ script.js
@@ -53,3 +53,25 @@ function creaTimer(time) {
 
 const time2s = creaTimer(2000);
 time2s()
+
+
+// SNACK5
+// Crea una funzione stampaOgniSecondo con setInterval.
+// Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+// Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+
+function stampaOgniSecondo(messaggio) {
+    return function () {
+        const intervallo = setInterval(() => {
+            console.log(messaggio)
+        }, 1000)
+
+        setTimeout(() => {
+            clearInterval(intervallo)
+        }, 4000)
+    }
+}
+
+const messaggio1 = stampaOgniSecondo("messaggio1")
+messaggio1()
+
